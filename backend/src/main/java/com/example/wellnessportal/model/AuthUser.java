1dbf48bde1af7e757a91760d583585b389084232
Kosuru <2438053@cognizant.com>
2025-09-10 14:55:56 +0530

    model/updatedentities

--- backend/src/main/java/com/example/wellnessportal/model/AuthUser.java
+++ backend/src/main/java/com/example/wellnessportal/model/AuthUser.java
@@ -1,8 +1,50 @@
 package com.example.wellnessportal.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 
 @Entity
 public class AuthUser {
 
+    @Id
+    private Long EmployeeId;
+    // Username can be customized by the employee but employee id is a mandatory field
+    
+    private String password;
+    //For Role-Based Login
+    private String role;
+    
+    public AuthUser(Long employeeId, 
+                     
+                    String password, 
+                    String role) {
+        this.EmployeeId = employeeId;
+        this.password = password;
+        this.role = role;
+    }
+    // Getters and setters
+    public Long getEmployeeId() {
+        return EmployeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.EmployeeId = employeeId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
--- backend/src/main/java/com/example/wellnessportal/model/Goal.java
+++ backend/src/main/java/com/example/wellnessportal/model/Goal.java
@@ -1,8 +1,93 @@
 package com.example.wellnessportal.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
 public class Goal {
 
+    @Id
+    private Long employeeId;
+    private String goalType;
+    private LocalDate targetDate;
+    private int status;
+    //Attributes For Performance Measurement API
+    private int targetScores;
+    private Rewards targetRewards;
+
+    public Goal(Long employeeId, 
+                String goalType,
+                LocalDate targetDate, 
+                int status, 
+                int targetScores, 
+                Rewards targetRewards) 
+                {
+        this.employeeId = employeeId;
+        this.goalType = goalType;
+        this.targetDate = targetDate;
+        this.status = status;
+        this.targetScores = targetScores;
+        this.targetRewards = targetRewards;
+    }
+    // Getters and setters
+    public Long getEmployeeId() 
+    {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) 
+    {
+        this.employeeId = employeeId;
+    }
+
+    public String getGoalType() 
+    {
+        return goalType;
+    }
+
+    public void setGoalType(String goalType) 
+    {
+        this.goalType = goalType;
+    }
+
+
+    public LocalDate getTargetDate() 
+    {
+        return targetDate;
+    }
+
+    public void setTargetDate(LocalDate targetDate) 
+    {
+        this.targetDate = targetDate;
+    }
+
+    public int getStatus() 
+    {
+        return status;
+    }
+
+    public void setStatus(int status) 
+    {
+        this.status = status;
+    }
+
+    public int getTargetScores() 
+    {
+        return targetScores;
+    }
+
+    public void setTargetScores(int targetScores) 
+    {
+        this.targetScores = targetScores;
+    }
+
+    public Rewards getTargetRewards() 
+    {
+        return targetRewards;
+    }
+
+    public void setTargetRewards(Rewards targetRewards) {
+        this.targetRewards = targetRewards;
+    }
 }
