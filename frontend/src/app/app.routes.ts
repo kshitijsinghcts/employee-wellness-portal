00ba53ff59fc4017af0c5422ad88edb12ce12b4b
Sarda, Mukund (Cognizant) <mukund.sarda@cognizant.com>
2025-09-23 09:47:53 +0530

    changes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { DashboardComponent } from './dashboard/dashboard';
+import { Goals } from './goals/goals';
 
 export const routes: Routes = [
     {path: '', component: Login},
-    {path: 'home', component: DashboardComponent}
+    {path: 'home', component: DashboardComponent},
+    {path: 'goals', component: Goals}
 ];
