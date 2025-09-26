5798596ac7021baf7b4a04576ce87740d5ca157c
Singh, Kshitij (Cognizant) <2438044@cognizant.com>
2025-09-26 09:07:32 +0530

    Documenting Repositories

--- backend/src/main/java/com/example/wellnessportal/repository/SurveyResponseRepository.java
+++ backend/src/main/java/com/example/wellnessportal/repository/SurveyResponseRepository.java
@@ -11,16 +11,15 @@ import com.example.wellnessportal.model.SurveyResponse;
 
 public interface SurveyResponseRepository extends JpaRepository<SurveyResponse, Long> {
 
+        @Query("SELECT sr FROM SurveyResponse sr WHERE sr.surveyId = :surveyId")
+        List<SurveyResponse> findSurveyResponseBySurveyId(@Param("surveyId") Long surveyId);
 
-    @Query("SELECT sr FROM SurveyResponse sr WHERE sr.surveyId = :surveyId")
-    List<SurveyResponse> findSurveyResponseBySurveyId(@Param("surveyId") Long surveyId);
+        @Query("SELECT sr FROM SurveyResponse sr WHERE sr.surveyId = :surveyId AND sr.employeeId = :employeeId")
+        SurveyResponse findSurveyResponseBySurveyIdAndEmployeeId(@Param("surveyId") Long surveyId,
+                        @Param("employeeId") Long employeeId);
 
-    @Query("SELECT sr FROM SurveyResponse sr WHERE sr.surveyId = :surveyId AND sr.employeeId = :employeeId")
-    SurveyResponse findSurveyResponseBySurveyIdAndEmployeeId(@Param("surveyId") Long surveyId,
-            @Param("employeeId") Long employeeId);
-
-    @Query("UPDATE SurveyResponse sr SET sr.answers = :newAnswers WHERE sr.surveyId = :surveyId OR sr.employeeId= :employeeId")
-    void updateSurveyBySurveyIdOrSurveyTitle(@Param("surveyId") Long surveyId,
-                                              @Param("employeeId") Long employeeId,
-                                              @Param("newAnswers") Map<String, String> newAnswers);
+        @Query("UPDATE SurveyResponse sr SET sr.answers = :newAnswers WHERE sr.surveyId = :surveyId OR sr.employeeId= :employeeId")
+        void updateSurveyBySurveyIdOrSurveyTitle(@Param("surveyId") Long surveyId,
+                        @Param("employeeId") Long employeeId,
+                        @Param("newAnswers") Map<String, String> newAnswers);
 }
